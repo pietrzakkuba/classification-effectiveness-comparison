b227e8b9da3c9994127ce8e6a3f8511d93c4d65a
pietrzakkuba <pietrzakkuba@yahoo.pl>
2021-12-19 18:22:34 +0100

    refactor

--- textcleaning.py
+++ textcleaning.py
@@ -3,8 +3,10 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
 import re
 import string
+import os
 
 stemmer = SnowballStemmer('english')
+
 
 def remove_punctuation(text: str) -> str:
     return ''.join([words for words in text if words not in string.punctuation])
@@ -26,25 +28,19 @@ def remove_multiple_whitespaces(text: str) -> str:
     return ' '.join(text.split())
 
 
-def remove_stopwords(text: str) -> str:
+def remove_stopwords_and_stem(text: str) -> list[str]:
     text_tokens = word_tokenize(text)
-    return ' '.join([word for word in text_tokens if word not in stopwords.words()])
+    return [stemmer.stem(word) for word in text_tokens if word not in stopwords.words()]
 
 
-def stem(text: str) -> str:
-    text_tokens = word_tokenize(text)
-    return ' '.join([stemmer.stem(word) for word in text_tokens])
-
-
-def clean_text(text: str) -> str:
+def clean_text_and_return_tokens(text: str) -> list[str]:
+    os.write(1, (text[:10] + '\n').encode())
     text = remove_links(text)
     text = remove_punctuation(text)
     text = lowercase(text)
     text = remove_words_containing_numbers(text)
     text = remove_multiple_whitespaces(text)
-    text = remove_stopwords(text)
-    text = stem(text)
-    return text
+    return remove_stopwords_and_stem(text)
 
 
 sample = 'Programmers You  234234   have won a Nokia 7250i. This is what you get when you win our FREE auction. To take part send Nokia to 86021 now. https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python/11332580'
